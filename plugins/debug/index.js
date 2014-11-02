5b61798f4109083499d30f69c29001b7c91a8e9a
Sascha Droste <pid@posteo.net>
2014-11-02 22:31:13 +0100

    add debug plugin

---
+++ plugins/debug/index.js
@@ -0,0 +1,36 @@
+console.log("PLUGIN: DEBUG loaded");
+
+var utils = require('../../lib/utils');
+var color = require('irc-colors');
+
+function init(client, config) {
+
+    client.addListener('message', function (nick, to, message, raw) {
+
+        if (!utils.isBotMessage(nick, config)) {
+            console.error("MESSAGE EVENT:", nick, to, message, raw);
+        }
+    });
+
+    client.addListener('join', function (channel, nick, raw) {
+        if (!utils.isBotMessage(nick, config)) {
+            console.error("JOIN EVENT:", channel, nick, raw);
+        }
+    });
+
+};
+
+function help() {
+    return (
+        color.bold.red.bgyellow('===================================================\n') +
+        color.bold.red.bgyellow('DEBUG Plugin\n') +
+        color.bold.red.bgyellow('===================================================\n') +
+        color.bold.red.bgyellow('Only for dev\n') +
+        color.bold.red.bgyellow('===================================================')
+    );
+}
+
+module.exports = {
+    init: init,
+    help: help
+}
