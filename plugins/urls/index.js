0c0d726d3a5874ca31d3db43822a147d5b68e188
Sascha Droste <pid@posteo.net>
2014-11-02 22:02:12 +0100

    add urls plugin

---
+++ plugins/urls/index.js
@@ -0,0 +1,89 @@
+console.log("PLUGIN: URLS loaded");
+
+var utils = require('../../lib/utils');
+var color = require('irc-colors');
+
+function init(client, config) {
+
+    var levelup = require('levelup');
+    var db = levelup(__dirname + '/data', {
+        keyEncoding: 'binary'
+    });
+    var Puid = require('puid');
+    var puid = new Puid(true);
+
+    client.addListener('message', function (nick, to, message, raw) {
+
+        console.log("URLS CMD:", nick, to, message, raw);
+
+        var cmdline = utils.filterCommands(message);
+        var maxLimit = 42;
+        var limit;
+
+        var urls = [];
+        var re = /http[s]?\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/g;
+
+        if (utils.isBotMessage(nick, config)) {
+
+            return;
+        }
+
+        if (utils.isBotMessage(to, config)) {
+            to = nick;
+        }
+
+        while (match = re.exec(message)) {
+            urls.push(match[0]);
+        }
+
+        urls.forEach(function (url) {
+            db.put('urls\x00' + puid.generate(), url, function (err) {
+
+                if (err) {
+                    console.log("ERROR: URLS: adding new url");
+                }
+            });
+        });
+
+        if (utils.isCmd('!urls', cmdline)) {
+
+            limit = cmdline.length > 1 ? parseInt(cmdline[1], 10) : 3;
+
+            db.createKeyStream({
+                    gt: 'urls\x00',
+                    lt: 'urls\x00\xff',
+                    limit: limit && limit <= maxLimit ? limit : 3,
+                    reverse: true
+                })
+                .on('data', function (data) {
+
+                    db.get(data, function (err, url) {
+
+                        sendMessage(client, to, url);
+                    });
+                });
+        }
+    });
+};
+
+function help() {
+    return (
+        color.bold.red.bgyellow('===================================================\n') +
+        color.bold.red.bgyellow('URLS Plugin\n') +
+        color.bold.red.bgyellow('===================================================\n') +
+        color.bold.red.bgyellow('!urls : get latest URLs\n') +
+        color.bold.red.bgyellow('!urls 5 : get the 5 latest URLs\n') +
+        color.bold.red.bgyellow('===================================================')
+    );
+}
+
+function sendMessage(client, to, message) {
+
+    client.say(to, color.blue('URL: ') + color.brown(message));
+    console.log('URLS: ', to, message);
+}
+
+module.exports = {
+    init: init,
+    help: help
+}
